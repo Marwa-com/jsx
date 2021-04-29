588da17392e5ad405515ac4d3d27e32381e7e723
Marwa <amri.maroia@yahoo.fr>
2021-04-29 02:48:14 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import myImage from "./techInt.jpg";
+import myVideo from "./significantTech.mp4";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div style={{borderStyle:'solid' ,borderSize:1 ,borderColor:'black' ,maxWidth:'100vw'}}>
+
+<h1 className='title red'> MARWA</h1> <br/>
+
+<img style={{width:350 ,height:250 }} src={myImage} alt ='techInt' /> <br/>
+
+<img style={{width:350 ,height:250 }} src="/technologieRFID.jpg" alt='technologieRFID'/> 
+
+</div>
+
+<video style={{width:320 ,height:240 }} controls autostart autoPlay src={myVideo} type="video/mp4" />
       </header>
     </div>
   );
